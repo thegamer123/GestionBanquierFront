75fc3bcf4a801c4831cc1c79902a2eff43a17927
Achref_Mahfoudh <a.Mahfoudh@bc-competence.de>
2021-07-25 21:11:26 +0100

    add user module

--- gestionBanquierFront/gestionBanquierFront/Controllers/UserController.cs
+++ gestionBanquierFront/gestionBanquierFront/Controllers/UserController.cs
@@ -1,89 +1,241 @@
-﻿using System;
+﻿using gestionBanquierFront.Models;
+using gestionBanquierFront.Utils;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace gestionBanquierFront.Controllers
 {
     public class UserController : Controller
     {
+
+        HttpClient httpClient;
+
+        public UserController()
+        {
+            httpClient = HttpClientAccessor.HttpClient;
+            //httpClient.BaseAddress = new Uri("http://localhost:8082/api/auth/");
+            // httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", (string)Session["accessToken"]);
+
+            ViewBag.userList = new SelectList(new List<SelectListItem>());
+        }
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", (string)Session["accessToken"]);
+        }
+
         // GET: User
         public ActionResult Index()
         {
+
+
+            var userListResponse = httpClient.GetAsync("auth/users").Result;
+
+            if (userListResponse.IsSuccessStatusCode)
+            {
+                var response = userListResponse.Content.ReadAsAsync<List<User>>().Result;
+                return View(response);
+            }
+
             return View();
         }
 
-        // GET: User/Details/5
-        public ActionResult Details(int id)
+        // GET: User/PDF/5
+
+        public ActionResult ExportPDF()
         {
-            return View();
-        }
+            var user = httpClient.GetAsync("auth/users/export/pdf").Result;
+               
+                var response = user.Content.ReadAsStringAsync().Result;
+            return File(response, "application/pdf", "report.pdf");
+            
 
-        // GET: User/Create
-        public ActionResult Create()
+            
+        }
+        public ActionResult ExportCSV()
         {
+            var user = httpClient.GetAsync("auth/users/export/csv").Result;
+            
+                var response = user.Content.ReadAsStringAsync().Result;
+                //string csv = ListToCSV(response);
+
+                return File(new System.Text.UTF8Encoding().GetBytes(response), "text/csv", "report.csv");
+            
+        }
+
+    
+    // GET: Account/Create
+    public ActionResult Create()
+        {
+            ViewBag.userList = new SelectList(Enumerable.Empty<User>());
+            var userList = httpClient.GetAsync("auth/users").Result;
+            if (userList.IsSuccessStatusCode)
+            {
+                var response = userList.Content.ReadAsAsync<List<User>>().Result;
+                var itemsList = new List<SelectListItem>();
+                response.ForEach(x =>
+                {
+                    itemsList.Add(new SelectListItem
+                    {
+                        Text = x.nom + " " + x.prenom,
+                        Value = x.id.ToString()
+                    });
+                });
+                ViewBag.userList = itemsList;
+            }
             return View();
         }
 
-        // POST: User/Create
+        // POST: Account/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public async Task<ActionResult> Create(FormCollection collection)
         {
             try
             {
-                // TODO: Add insert logic here
+                var provider = CultureInfo.InvariantCulture;
+                var user = new CreateUserModel
+                {
 
-                return RedirectToAction("Index");
+                    nom = collection["nom"],
+                    prenom = collection["prenom"],
+
+                    email = collection["email"],
+
+                    username = collection["username"],
+
+                    password = collection["password"],
+
+                };
+
+
+
+                //string jsonString = JsonConvert.SerializeObject(account);
+
+                var response = await httpClient.PostAsJsonAsync("auth/register/", user);
+
+                if (response.IsSuccessStatusCode)
+
+                {
+                    var result = response.Content.ReadAsStringAsync();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    response.EnsureSuccessStatusCode();
+                    return View();
+                }
+
+
+
+
+
             }
-            catch
+            catch (Exception e)
             {
-                return View();
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                return View("Index");
             }
         }
 
-        // GET: User/Edit/5
+        // POST: Account/Edit/5
+        [HttpGet]
         public ActionResult Edit(int id)
         {
+            var userListResponse = httpClient.GetAsync("auth/users/{id}"+id).Result;
+
+            if (userListResponse.IsSuccessStatusCode)
+            {
+                var response = userListResponse.Content.ReadAsAsync<User>().Result;
+                return View(response);
+            }
+
             return View();
         }
 
-        // POST: User/Edit/5
+        // GET: Account/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
-            try
+            ViewBag.userList = new SelectList(Enumerable.Empty<User>());
+            var userList = httpClient.GetAsync("auth/users/{id}"+id).Result;
+            if (userList.IsSuccessStatusCode)
             {
-                // TODO: Add update logic here
+                var response = userList.Content.ReadAsAsync<List<User>>().Result;
+                var itemsList = new List<SelectListItem>();
+                response.ForEach(x =>
+                {
+                    itemsList.Add(new SelectListItem
+                    {
+                        Text = x.nom + " " + x.prenom,
+                        Value = x.id.ToString()
+                    });
+                });
+                ViewBag.userList = itemsList;
+            }
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return View(userList);
+
         }
-
-        // GET: User/Delete/5
-        public ActionResult Delete(int id)
+        // GET: Uerr/Details/5
+        public ActionResult Details(int id)
         {
+
+            var userListResponse = httpClient.GetAsync("auth/users/"+id).Result;
+
+            if (userListResponse.IsSuccessStatusCode)
+            {
+                var response = userListResponse.Content.ReadAsAsync<List<User>>().Result;
+                return View(response);
+            }
+
             return View();
         }
 
-        // POST: User/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
+
+        // POST: Account/Delete/5
+        [HttpPost]
+        public async Task<ActionResult> Delete(int id, FormCollection collection)
+        {
             {
-                return View();
+                try
+                {
+
+                    var response = await httpClient.PostAsync("auth/deleteusers/{id}" + id, null);
+
+                    if (response.IsSuccessStatusCode)
+
+                    {
+                        var result = response.Content.ReadAsStringAsync();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        response.EnsureSuccessStatusCode();
+                        return View();
+                    }
+
+
+                }
+                catch
+                {
+                    return View();
+                }
             }
         }
     }
 }
+
---
+++ gestionBanquierFront/gestionBanquierFront/Models/CreateUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gestionBanquierFront.Models
+{
+    public class CreateUserModel
+    {
+        public string nom { get; set; }
+
+        public string prenom { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+    }
+}
